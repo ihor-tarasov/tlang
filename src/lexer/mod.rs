c41f9576ce9493880dbc4a21342ee1181d5c01be
ihor-tarasov <mov.ax.38@gmail.com>
2023-06-13 00:39:50 +0300

    Support of whitespaces

--- src/lexer/mod.rs
+++ src/lexer/mod.rs
@@ -4,8 +4,22 @@ mod reader;
 
 pub use reader::*;
 
-pub fn lex<R: Reader>(reader: &mut R) -> Token {
+fn is_whitespace(c: u8) -> bool {
+    c == b' ' || c == b'\t' || c == b'\r' || c == b'\n'
+}
 
+fn skip_whitespaces<R: Reader>(reader: &mut R) {
+    while let Some(c) = reader.current() {
+        if is_whitespace(c) {
+            reader.next();
+        } else {
+            break;
+        }
+    }
+}
+
+pub fn lex<R: Reader>(reader: &mut R) -> Token {
+    skip_whitespaces(reader);
 
     let start = reader.pos();
 
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 
 fn main() {
-    if let Some(v) = tlang::run_str_unwrap("2+2-1+ 8-9+4") {
+    if let Some(v) = tlang::run_str_unwrap("2 + 2 - 1 + 8 - 9 + 4") {
         println!("{v:?}");
     }
 }
